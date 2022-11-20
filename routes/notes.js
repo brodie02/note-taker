2ce2fac7e7c04d0299a884ca16d4d7bfe8933693
brodie02 <marshallbrodie0@gmail.com>
2022-11-20 17:33:31 +1030

    Add notes route

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,12 @@
 const notes = require('express').Router()
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+)
+
+notes.post('/', (req, res) => {
+    res.json('post')
+})
+
+module.exports = notes
