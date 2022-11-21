3533920e7ca378e079aa3624fa21695eb305129a
brodie02 <marshallbrodie0@gmail.com>
2022-11-21 15:31:22 +1030

    Add unique id package

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,13 @@
 const notes = require('express').Router()
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 )
 
 notes.post('/', (req, res) => {
-    res.json('post')
+    
 })
 
 module.exports = notes
